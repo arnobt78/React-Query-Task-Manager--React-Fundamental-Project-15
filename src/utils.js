5e89f472cde0fc20d87292ffb465f0fe877a22cd
Arnob Mahmud <arnob_t78@yahoo.com>
2025-02-10 15:25:42 +0600

    update base url for Render-deployment

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,14 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// const customFetch = axios.create({
+//   baseURL: 'http://localhost:5000/api/tasks',
+// });
+
+// export default customFetch;
+import axios from "axios";
 
 const customFetch = axios.create({
-  baseURL: 'http://localhost:5000/api/tasks',
+  baseURL: "https://task-management-server-nyfr.onrender.com/api/tasks",
 });
 
 export default customFetch;
